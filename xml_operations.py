f770d995225c674377cf653c3b297183a4f7b430
Piotr Szmigielski <piotrek.szmigielski123@wp.pl>
2025-06-04 23:56:54 +0200

    Dodano zapisywanie XML (Task7)

--- xml_operations.py
+++ xml_operations.py
@@ -10,8 +10,23 @@ def load_xml(file_path):
         print("BŁĄD XML: {e}")
         return None
 
+def save_xml(data, file_path):
+    try:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(xmltodict.unparse(data, pretty=True))  # pretty=True dla czytelności
+            print("Dane zapisano poprawnie do pliku {file_path}!")
+        return True
+    except Exception as e:
+        print("BŁĄD przy zapisie XML: {e}")
+        return False
+
 if __name__ == "__main__":
-    sciezka = input("Podaj ścieżkę do pliku XML: ")
-    dane = load_xml(sciezka)
+    # Test wczytywania
+    sciezka_wejsciowa = input("Podaj ścieżkę do pliku XML (do wczytania): ")
+    dane = load_xml(sciezka_wejsciowa)
+    
     if dane:
         print("Zawartość pliku:", dane)
+        # Test zapisywania
+        sciezka_wyjsciowa = input("Podaj ścieżkę do nowego pliku XML (do zapisu): ")
+        save_xml(dane, sciezka_wyjsciowa)
