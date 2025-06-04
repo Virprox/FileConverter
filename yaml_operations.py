e1c1219f259ec1a912ecbaffeedf0bc7f620e8db
Piotr Szmigielski <piotrek.szmigielski123@wp.pl>
2025-06-04 23:51:01 +0200

    Dodano zapisywanie YAML (Task5)

--- yaml_operations.py
+++ yaml_operations.py
@@ -13,8 +13,23 @@ def load_yaml(file_path):
         print("BŁĄD: Nie ma takiego pliku!")
         return None
 
+def save_yaml(data, file_path):
+    try:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            yaml.dump(data, file, default_flow_style=False, allow_unicode=True)
+            print(f"Dane zapisano poprawnie do pliku {file_path}!")
+        return True
+    except Exception as e:
+        print(f"BŁĄD przy zapisie YAML: {e}")
+        return False
+
 if __name__ == "__main__":
-    sciezka = input("Podaj ścieżkę do pliku YAML: ")
-    dane = load_yaml(sciezka)
+    # Test wczytywania
+    sciezka_wejsciowa = input("Podaj ścieżkę do pliku YAML (do wczytania): ")
+    dane = load_yaml(sciezka_wejsciowa)
+    
     if dane:
         print("Zawartość pliku:", dane)
+        # Test zapisywania
+        sciezka_wyjsciowa = input("Podaj ścieżkę do nowego pliku YAML (do zapisu): ")
+        save_yaml(dane, sciezka_wyjsciowa)
